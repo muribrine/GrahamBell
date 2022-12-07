000508a7590be062da4c810f329b591df8c087ca
muribrine <muribrine@gmail.com>
2022-12-07 20:01:53 -0300

    major changes
    1. Completed Front-End code to send and receive messages to and from backend.
    2. Backend now sends previous messages to newly connected clients and mediates data transfer between all clients.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 function createServer() {
 	try {
 		/* --- dependencies ---*/
-		console.log('starting server... loading dependencies...');
+		console.log('Starting server... loading dependencies...');
 		const express = require('express');
 		const ejs = require('ejs');
 		const http = require('http');
 		const socketIO = require('socket.io');
 		const path = require('path');
-		console.log('depencies loaded successfully.');
+		console.log('Depencies loaded successfully.');
 		/* --- --- ---*/
 
 		/* --- Parameters ---*/
@@ -27,8 +27,8 @@ function createServer() {
 		console.log('Protocols defined successfully.');
 		/* --- --- ---*/
 
-		/* --- server setup---*/
-		console.log('setting up server...');
+		/* --- server code---*/
+		console.log('Setting up server...');
 		app.use(express.static(publicPath));
 		app.set('views', publicPath);
 		app.engine('html', ejs.renderFile);
@@ -38,8 +38,23 @@ function createServer() {
 			res.render('app.html');
 		});
 
-		console.log('Server setup completed. Listening to port ' + port);
-		console.log('http://localhost:' + port + '/');
+		let messages = [];
+
+		io.on('connection', (socket) => {
+			console.log(`Connecting socket... SocketID: ${socket.id}`);
+
+			socket.emit('previousMessages', messages);
+			console.log(`Sending previous messages to socket ${socket.id}`);
+
+			socket.on('sendMessage', (data) => {
+				messages.push(data);
+				console.log('Sending new message data to all clients...');
+				socket.broadcast.emit('receivedMessage', data);
+			});
+		});
+
+		console.log(`Server setup completed. Listening to port: ${port}`);
+		console.log(`http://localhost:${port}/`);
 
 		server.listen(port);
 		/* --- --- ---*/
